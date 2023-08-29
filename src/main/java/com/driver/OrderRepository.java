457bb4e2c2d6d2921c28a00512428a98aa4f7436
Deepesh Gupta <guptad28031998@gmail.com>
2023-08-29 11:55:34 +0530

    d1t9

--- src/main/java/com/driver/OrderRepository.java
+++ src/main/java/com/driver/OrderRepository.java
@@ -103,11 +103,10 @@ public class OrderRepository {
         List<Order> orderList = listHashMap.getOrDefault(partnerId, new ArrayList<>());
         for (Order order : orderList) {
             int deliveryTimeInMinutes = order.getDeliveryTime();
-            if (deliveryTimeInMinutes > targetTimeInMinutes) {
+            if (deliveryTimeInMinutes >= targetTimeInMinutes) {
                 ordersLeft++;
             }
         }
-        ordersLeft += orderHashMap.size();
         Integer integer=ordersLeft;
         return integer;
     }
